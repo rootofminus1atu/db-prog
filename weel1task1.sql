dc901952fa63e10ae61d998337c5193d819c066e
RootOfMinus1ATU <s00240122@atu.ie>
2025-01-25 14:37:57 +0000

    Create weel1task1.sql

---
+++ weel1task1.sql
@@ -0,0 +1,33 @@
+-- task:
+-- create a proc that will tell a customer if they have enough credits to order X amount of a particular product
+--
+CREATE PROC Name
+	@ECustomerId INT,
+	@EProductId INT,
+	@EAmount INT
+AS
+BEGIN
+	DECLARE @ICreditLimit INT;
+	DECLARE @IProductPrice INT;
+	DECLARE @ITotalPrice INT;
+	DECLARE @IMessage VARCHAR(255);
+	
+	-- find the credit limit for @ECustomerId
+	SELECT @ICreditLimit = CreditLimit
+	FROM CustomerTBL
+	WHERE CustomerId = @ECustomerId;
+	
+	-- find the price for @EProductId
+	SELECT @IProductPrice = Price
+	FROM ProductTBL
+	WHERE ProductId = @EProductId;
+	
+	-- calculate the total price
+	SET @ITotalPrice = Price * @EAmount;
+	
+	-- compare them
+	SET @IMessage = IIF(@ITotalPrice > @ICreditLimit, 'You don\'t have enough credits', 'You have enough credits');
+	
+	-- return true/false as a message
+	SELECT @IMessage;
+END
